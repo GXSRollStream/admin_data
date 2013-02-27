bc2bb566a5fe728b7728d95e390afd410f61a02d
Subba Rao Pasupuleti <subbarao.pasupuleti@gmail.com>
2013-02-27 09:20:57 -0500

    remove layout from home

--- app/controllers/admin_data/home_controller.rb
+++ app/controllers/admin_data/home_controller.rb
@@ -1,13 +1,9 @@
 module AdminData
   class HomeController < ApplicationController
-
-    layout 'admin_data'
-
     def index
       respond_to do |format|
         format.html
       end
     end
-
   end
 end
